e1f6eff434a9d103c3ddb1d2b0a068ba4b4cc0d5
saadia <selachguir@gmail.com>
2023-12-29 09:58:07 +0100

    add breez to breez branch

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ClientController;
-use App\Http\Controllers\ProduitController;
-use App\Http\Controllers\CommandeController;
-use App\Http\Controllers\CategorieController;
-use App\Http\Controllers\LigneCommandeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,27 +14,18 @@ use App\Http\Controllers\LigneCommandeController;
 |
 */
 
-Route::get('/',[HomeController::class,'index'])->name('home.index');
-Route::get('/add/{id}',[HomeController::class,'add'])->name('home.add');
-Route::get('/show',[HomeController::class,'show'])->name('home.show');
-Route::get('/destroy/{id}',[HomeController::class,"destroy"])->name('home.destroy'); 
-Route::get('/clear',[HomeController::class,"clear"])->name('home.clear'); 
-Route::get('/search',[HomeController::class,"search"])->name('home.search'); 
+Route::get('/', function () {
+    return view('welcome');
+});
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get("categorie/search",[CategorieController::class,'search'])->name("categories.search");
-Route::get("categorie/clear",[CategorieController::class,'clear'])->name("categories.clear");
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get("produit/search",[ProduitController::class,'search'])->name("produits.search");
-Route::get("produit/clear",[ProduitController::class,'clear'])->name("produits.clear");
-
-
-Route::get("/myOrders",[CommandeController::class,"myOrders"])->name("commandes.myOrders");
-Route::post('/commandes/exportCSV',[CommandeController::class,"exportCSV"])->name("commandes.exportCSV");
-
-
-Route::resource("categories",CategorieController::class);
-Route::resource("produits",ProduitController::class);
-Route::resource("clients",ClientController::class);
-Route::resource("commandes",CommandeController::class);
-Route::resource("ligneCommandes",LigneCommandeController::class);
+require __DIR__.'/auth.php';
